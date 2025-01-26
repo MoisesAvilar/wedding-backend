af6fae80c004e664e175a54958a55dbd6203648f
MoisesAvilar <moisesavilar0@gmail.com>
2025-01-26 10:41:54 -0300

    some changes too

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configuração de CORS
 const corsOptions = {
-    origin: 'https://ar-wedding-nu.vercel.app', // Substitua pelo domínio do front-end
+    origin: 'https://ar-wedding-nu.vercel.app', // Domínio do front-end
     methods: ['GET', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
 };
-
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
@@ -61,39 +58,26 @@ const presentes = [
     { id: 41, nome: 'Ferro de Passar', disponivel: true }
   ];
 
-// Endpoint para obter todos os presentes
+// Endpoint para listar todos os presentes
 app.get('/presentes', (req, res) => {
-    res.json(presentes); // Retorna os presentes diretamente
+    res.json(presentes);
 });
 
-// Endpoint para obter um presente específico
-app.get('/presentes/:id', (req, res) => {
-    const { id } = req.params; // Pega o ID da URL
-    const presente = presentes.find(p => p.id == id); // Busca o presente com o ID fornecido
-
-    if (!presente) {
-        return res.status(404).json({ message: 'Presente não encontrado!' });
-    }
-
-    res.json(presente); // Retorna o presente encontrado
-});
-
-// Endpoint para alterar a disponibilidade de um presente
+// Endpoint para atualizar a disponibilidade de um presente
 app.patch('/presentes/:id', (req, res) => {
     const { id } = req.params;
     const { disponivel } = req.body;
 
-    let presente = presentes.find(p => p.id == id);
-
+    const presente = presentes.find(p => p.id == id);
     if (!presente) {
         return res.status(404).json({ message: 'Presente não encontrado!' });
     }
 
-    // Atualiza a disponibilidade
     presente.disponivel = disponivel;
+    res.json(presente);
 });
 
-// Iniciar servidor
+// Inicia o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
